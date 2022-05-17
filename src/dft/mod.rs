46aac87040c531fa1b9e490aa6bb686c8410ed0c
Rolf Stierle <RolfStierle@users.noreply.github.com>
2022-05-17 17:37:39 +0200

    Correct pair potential (#5)
    * PairPotential corrected for LJTS.
    
    * Test added.
    
    Co-authored-by: Rolf Stierle <stierle@itt.uni-stuttgart.de>

--- src/dft/mod.rs
+++ src/dft/mod.rs
@@ -7,7 +7,7 @@ use feos_core::{IdealGasContribution, MolarWeight};
 use feos_dft::adsorption::FluidParameters;
 use feos_dft::fundamental_measure_theory::{FMTContribution, FMTProperties, FMTVersion};
 use feos_dft::solvation::PairPotential;
-use feos_dft::{FunctionalContribution, HelmholtzEnergyFunctional, DFT, MoleculeShape};
+use feos_dft::{FunctionalContribution, HelmholtzEnergyFunctional, MoleculeShape, DFT};
 use ndarray::{Array, Array1, Array2};
 use num_dual::DualNum;
 use pure_pets_functional::*;
@@ -142,9 +142,52 @@ impl FluidParameters for PetsFunctional {
 impl PairPotential for PetsFunctional {
     fn pair_potential(&self, r: &Array1<f64>) -> Array2<f64> {
         let sigma = &self.parameters.sigma;
+        let rc = 2.5 * sigma;
+        let u_rc = 4.0
+            * &self.parameters.epsilon_k
+            * ((sigma / &rc).mapv(|l| l.powi(12)) - (sigma / &rc).mapv(|l| l.powi(6)));
+
         Array::from_shape_fn((self.parameters.sigma.len(), r.len()), |(i, j)| {
-            4.0 * self.parameters.epsilon_k[i]
-                * ((sigma[i] / r[j]).powi(12) - (sigma[i] / r[j]).powi(6))
+            if r[j] > rc[i] {
+                0.0
+            } else {
+                4.0 * self.parameters.epsilon_k[i]
+                    * ((sigma[i] / r[j]).powi(12) - (sigma[i] / r[j]).powi(6))
+                    - u_rc[i]
+            }
         })
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use crate::parameters::utils::{argon_krypton_parameters, argon_parameters};
+
+//     use approx::assert_relative_eq;
+//     use ndarray::Array;
+//     use std::error::Error;
+
+//     #[test]
+//     #[allow(non_snake_case)]
+//     fn test_pair_potential() -> Result<(), Box<dyn Error>> {
+//         let params_pure = argon_parameters();
+//         let params_binary = argon_krypton_parameters();
+//         let func_pure = Rc::new(PetsFunctional::new(params_pure));
+//         let func_binary = Rc::new(PetsFunctional::new(params_binary));
+
+//         let r = Array::linspace(2.5, 17.5, 151);
+
+//         println!("pure {}", func_pure.pair_potential(&r),);
+//         println!("binary {}", func_binary.pair_potential(&r),);
+
+//         // assert_relative_eq!(
+//         //     vle_pure.vapor().density,
+//         //     vapor_density,
+//         //     max_relative = 1e-13,
+//         // );
+
+//         panic!();
+//         Ok(())
+//     }
+// }
